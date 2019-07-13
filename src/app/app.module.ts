f893d5362e815f346d369cd52ac87c0b3e7835a7
Ran Har-Shuv <ran.harshuv@gmail.com>
2019-07-13 20:41:44 +0300

    add sample routings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+
+const appRoute: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'user', component: UserComponent},
+  { path: 'users', component: UsersComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    HomeComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
